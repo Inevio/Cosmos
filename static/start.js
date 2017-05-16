6a2e83a122960d3701e9538f20f85a600fae94fb
Jorge Fernandez <jorge92f@gmail.com>
2017-05-16 12:59:12 +0200

    fixes

--- static/start.js
+++ static/start.js
@@ -6,7 +6,7 @@ var parameters;
 
 parameters = params
 
-if( api.app.getViews('main').length === 1 || app.hasClass( 'wz-mobile-view' ) ){
+if( api.app.getViews('main').length === 1 || app.hasClass( 'wz-mobile-view' ) || !parameters ){
 
   wz.cosmos.getUserWorlds( myContactID , {from:0 , to:1000} , function( e , o ){
 
