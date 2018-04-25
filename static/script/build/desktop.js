f369f3aa726522c957bf99395f89deb712133974
Carlos Navas <carlosnavas1991@gmail.com>
2018-04-25 11:43:23 +0200

    Update post fix

--- static/script/build/desktop.js
+++ static/script/build/desktop.js
@@ -2107,6 +2107,9 @@ var model = (function (view) {
           //this.showPosts(post.worldId, 0)
           this.view.prependPost(this.worlds[post.worldId].posts[ post.id ])
         }
+        this.worlds[post.worldId].posts[ post.id ].loadPostFsnodes( function(updatedPost){
+          this.updatePostFSNodes(updatedPost)
+        }.bind(this))
       }
     }
 
