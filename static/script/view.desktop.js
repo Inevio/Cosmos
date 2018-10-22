c51fa6814a3f840f250f67cd1ed2df47d6ac6f33
Carlos Navas <carlosnavas1991@gmail.com>
2018-10-22 12:39:40 +0200

    No posts fix

--- static/script/view.desktop.js
+++ static/script/view.desktop.js
@@ -1589,6 +1589,8 @@ var view = (function () {
     }
 
     prependPost (post) {
+      this._noPosts.css('opacity', '0')
+      this._noPosts.hide()
       this.appendPost( post, null, function(postDom){
         $('.you-card.wz-prototype').after(postDom)
       })
